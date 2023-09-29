3235747e2f84d0140e26904bdd1b8273bf5f1c51
Lakshaya Krishnaraj <lakshayakrishnaraj7@gmail.com>
2023-09-29 22:06:26 +0530

    creating js file and adding element selection

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const task_input = document.querySelector("input");
+const date_input = document.querySelector(".schedule-date"); // added date input
+const add_btn = document.querySelector(".add-task-button");
+const todos_list_body = document.querySelector(".todos-list-body");
+const alert_message = document.querySelector(".alert-message");
+const delete_all_btn = document.querySelector(".delete-all-btn");
