0fe925f7950451435c9af34b4129044e6c3c6af2
Lakshaya Krishnaraj <lakshayakrishnaraj7@gmail.com>
2023-09-29 22:11:58 +0530

    DOMContentLoaded Event Listen Random ID Generation

--- js/main.js
+++ js/main.js
@@ -6,3 +6,19 @@ const alert_message = document.querySelector(".alert-message");
 const delete_all_btn = document.querySelector(".delete-all-btn");
 
 let todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+window.addEventListener("DOMContentLoaded", () => {
+  showAllTodos();
+  if (!todos.length) {
+    displayTodos([]);
+  }
+});
+
+//get random unique id
+function getRandomId() {
+  return (
+    Math.random().toString(36).substring(2, 15) +
+    Math.random().toString(36).substring(2, 15)
+  );
+}
+
