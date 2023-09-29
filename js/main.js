e709eaca42da8b2ffa9b616eeaaf4139913f5d29
Lakshaya Krishnaraj <lakshayakrishnaraj7@gmail.com>
2023-09-29 22:07:54 +0530

    Todos Array

--- js/main.js
+++ js/main.js
@@ -4,3 +4,5 @@ const add_btn = document.querySelector(".add-task-button");
 const todos_list_body = document.querySelector(".todos-list-body");
 const alert_message = document.querySelector(".alert-message");
 const delete_all_btn = document.querySelector(".delete-all-btn");
+
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
