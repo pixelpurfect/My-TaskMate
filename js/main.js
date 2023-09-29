0e0f0fd554f4d2f2db96aae9edbef1eb03ebb486
Lakshaya Krishnaraj <lakshayakrishnaraj7@gmail.com>
2023-09-29 22:13:46 +0530

    more features

--- js/main.js
+++ js/main.js
@@ -22,3 +22,45 @@ function getRandomId() {
   );
 }
 
+function addToDo(task_input, date_input) {
+  let task = {
+    id: getRandomId(),
+    task: task_input.value,
+    dueDate: date_input.value, // added due date
+    completed: false,
+    status: "pending", // adding initial status as 'pending'
+  };
+  todos.push(task);
+}
+
+task_input.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13 && task_input.value.length > 0) {
+    addToDo(task_input, date_input); // Added date input
+    saveToLocalStorage();
+    task_input.value = "";
+    showAllTodos();
+  }
+});
+
+add_btn.addEventListener("click", () => {
+  if (task_input.value === "") {
+    showAlertMessage("Please enter a task", "error");
+  } else {
+    addToDo(task_input, date_input); // Added date input
+    saveToLocalStorage();
+    showAllTodos();
+    task_input.value = "";
+    date_input.value = ""; // Added date input
+    showAlertMessage("Task added successfully", "success");
+  }
+});
+
+delete_all_btn.addEventListener("click", clearAllTodos);
+
+//show all todos
+function showAllTodos() {
+  todos_list_body.innerHTML = "";
+  if (todos.length === 0) {
+    todos_list_body.innerHTML = `<tr><td colspan="5" class="text-center">No task found</td></tr>`;
+    return;
+  }
